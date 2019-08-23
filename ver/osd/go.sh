ebee5f244894d0258cf46171bbf4e111ea5fe71a
Jose Tejada <jotego@gmail.com>
2019-08-23 08:00:00 +0200

    MiSTer OSD backimage fixed for non-rotated display

--- ver/osd/go.sh
+++ ver/osd/go.sh
@@ -1,3 +1,11 @@
 #!/bin/bash
 
-iverilog -d SIMULATION test.v ../../hdl/mister/sys/osd.sv -g2005-sv -o sim && sim -lxt
+iverilog -DSIMULATION -DDUMP test.v ../../hdl/mister/sys/osd.sv -g2005-sv -o sim && sim -lxt
+
+echo "Converting the video output to video.png"
+octave << 'EOF'
+load video_dump.m
+imwrite(video_dump,"video.png")
+exit
+EOF
+
