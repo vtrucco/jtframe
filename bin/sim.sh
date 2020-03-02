9df84ef1cbf7ec5f951d47a857ce7edefbc493fc
jotego <jotego@gmail.com>
2020-03-02 07:44:00 +0100

    minor

--- bin/sim.sh
+++ bin/sim.sh
@@ -462,7 +462,7 @@ verilator)
         $MAXFRAME -DSIM_MS=$SIM_MS --lint-only $EXTRA;;
 esac
 
-if [ "$VIDEO_DUMP" = TRUE ]; then
+if [[ "$VIDEO_DUMP" = TRUE && -e video.raw ]]; then
 # convert -size 384x240 -depth 8 RGBA:video.raw -resize 200% video.png
     convert $CONVERT_OPTIONS -size ${VIDEOWIDTH}x${VIDEOHEIGHT} \
         -depth 8 RGBA:video.raw video.jpg
