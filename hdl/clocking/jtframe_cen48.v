9a7567ef950fdf8e080b58cee43af0bac239a63d
jotego <jotego@gmail.com>
2020-03-19 10:42:17 +0100

    Added cen4 output aligned with cen12

--- hdl/clocking/jtframe_cen48.v
+++ hdl/clocking/jtframe_cen48.v
@@ -26,6 +26,7 @@ module jtframe_cen48(
     output  reg cen8,
     output  reg cen6,
     output  reg cen4,
+    output  reg cen4_12, // cen4 based on cen12
     output  reg cen3,
     output  reg cen3q, // 1/4 advanced with respect to cen3
     output  reg cen1p5,
@@ -40,6 +41,7 @@ module jtframe_cen48(
 reg [4:0] cencnt=5'd0;
 reg [2:0] cencnt6=3'd0;
 reg       cencnt12=1'd0;
+reg [2:0] cencnt4_12=3'd1;
 
 always @(posedge clk) begin
     cencnt  <= cencnt+5'd1;
@@ -50,6 +52,8 @@ end
 always @(posedge clk) begin
     cen12  <= cencnt[1:0] == 2'd0;
     cen12b <= cencnt[1:0] == 2'd2;
+    if(cencnt[1:0]==2'b0) cencnt4_12 <= { cencnt4_12[1:0], cencnt4_12[2]};
+    cen4_12 <= cencnt[1:0]==2'd0 && cencnt4_12[2];
     cen16  <= cencnt6 == 3'd0 || cencnt6 == 3'd3;
     cen8   <= cencnt6     == 3'd0;
     cen4   <= cencnt6     == 3'd0 && cencnt12;
