46940387c9ca91e732d85192b5077f684cffe77d
jotego <jotego@gmail.com>
2020-04-16 10:27:48 +0200

    hex2bin converts 16-bit hex files to binary

--- cc/hex2bin.cc
+++ cc/hex2bin.cc
@@ -1,18 +1,30 @@
+// Converts 16-bit hex file to binary
+
 #include <iostream>
-#include <iomanip>
 #include <sstream>
 #include <string>
+#include <iomanip>
 
 using namespace std;
 
+#define BUFSIZE 4096
+
 int main( int argc, char *argv[] ) {
+    char *buf=new char[BUFSIZE];
+    int k=0;
     while(!cin.eof()) {
         int x;
         string s;
-        getline(s,cin);
+        getline(cin,s);
         stringstream ss(s);
         ss >> hex >> x;
-        char b = (char)(x&0xff);
-        cout.write(&b,1);
+        buf[k++] = (char)(x&0xff);
+        buf[k++] = (char)((x>>8)&0xff);
+        if(k==BUFSIZE) {
+            cout.write( buf,BUFSIZE);
+            k=0;
+        }
     }
+    delete buf;
+    return 0;
 }
