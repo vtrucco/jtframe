40d2024d74bacf283796d9a4c04cfce4206b64ee
jotego <jotego@gmail.com>
2020-07-09 14:14:36 +0200

    jtframe_mixer modified to normalize the inputs first

--- hdl/sound/jtframe_mixer.v
+++ hdl/sound/jtframe_mixer.v
@@ -20,48 +20,57 @@
     
 */
 
-// Generic mixer: same as jt12_mixer in JT12 repository
-// wout should be larger or equal than any input (w0,w1,w2,w3)
+// Generic mixer: improves on the jt12_mixer in JT12 repository
 
-module jtframe_mixer #(parameter w0=16,w1=16,w2=16,w3=16,wout=20)(
+// Usage:
+// Specify width of input signals and desired outputs
+// Select gain for each signal
+
+module jtframe_mixer #(parameter W0=16,W1=16,W2=16,W3=16,WOUT=16)(
     input                    clk,
     input                    cen,
     // input signals
-    input  signed [w0-1:0]   ch0,
-    input  signed [w1-1:0]   ch1,
-    input  signed [w2-1:0]   ch2,
-    input  signed [w3-1:0]   ch3,
+    input  signed [W0-1:0]   ch0,
+    input  signed [W1-1:0]   ch1,
+    input  signed [W2-1:0]   ch2,
+    input  signed [W3-1:0]   ch3,
     // gain for each channel in 4.4 fixed point format
     input  [7:0]             gain0,
     input  [7:0]             gain1,
     input  [7:0]             gain2,
     input  [7:0]             gain3,
-    output reg signed [wout-1:0] mixed
+    output     signed [WOUT-1:0] mixed
 );
+
+localparam WM = 16;
+localparam WA = WM+8; // width for the amplification
+localparam WS = WA+3; // width for the sum
+localparam signed [WM+3:0] MAXPOS = {  5'b0, {WM-1{1'b1}}};
+localparam signed [WM+3:0] MAXNEG = { ~5'b0, {WM-1{1'b0}}};
+
 
 `ifdef SIMULATION
 initial begin
-    if( wout<w0 || wout<w1 || wout<w2 || wout<w3 ) begin
-        $display("ERROR: %m parameter wout must be larger than any other w parameter");
+    if( WOUT<W0 || WOUT<W1 || WOUT<W2 || WOUT<W3 ) begin
+        $display("ERROR: %m parameter WOUT must be larger or equal than any other w parameter");
+        $finish;
+    end
+    if( W0>WM || W1 > WM || W2>WM || W3>WM || WOUT>WM ) begin
+        $display("ERROR: %m parameters cannot be larger than %d bits",WM);
         $finish;
     end
 end
 `endif
 
-reg signed [w0+7:0] ch0_amp;
-reg signed [w1+7:0] ch1_amp;
-reg signed [w2+7:0] ch2_amp;
-reg signed [w3+7:0] ch3_amp;
+wire signed [WM+7:0] ch0_pre, ch1_pre, ch2_pre, ch3_pre;
+wire signed [WM+3:0] pre_sum;
+reg  signed [WM-1:0] ch0_amp, ch1_amp, ch2_amp, ch3_amp, sum;
 
-// rescale to wout+4+8
-wire signed [wout+11:0] scaled0 = { {wout+4-w0{ch0_amp[w0+7]}}, ch0_amp   };
-wire signed [wout+11:0] scaled1 = { {wout+4-w1{ch1_amp[w1+7]}}, ch1_amp   };
-wire signed [wout+11:0] scaled2 = { {wout+4-w2{ch2_amp[w2+7]}}, ch2_amp   };
-wire signed [wout+11:0] scaled3 = { {wout+4-w3{ch3_amp[w3+7]}}, ch3_amp   };
-
-reg signed [wout+11:0] sum, limited;
-
-wire signed [wout+11:0] max_pos = { {13{1'b0}}, {(wout-1){1'b1}}};
+// rescale to WM
+wire signed [WM-1:0] scaled0 = { ch0, {WM-W0{1'b0}} };
+wire signed [WM-1:0] scaled1 = { ch1, {WM-W1{1'b0}} };
+wire signed [WM-1:0] scaled2 = { ch2, {WM-W2{1'b0}} };
+wire signed [WM-1:0] scaled3 = { ch3, {WM-W3{1'b0}} };
 
 wire signed [8:0]
     g0 = {1'b0, gain0},
@@ -69,33 +78,37 @@ wire signed [8:0]
     g2 = {1'b0, gain2},
     g3 = {1'b0, gain3};
 
+assign ch0_pre = g0 * scaled0;
+assign ch1_pre = g1 * scaled1;
+assign ch2_pre = g2 * scaled2;
+assign ch3_pre = g3 * scaled3;
+assign pre_sum = (ch0_pre + ch1_pre + ch2_pre + ch3_pre)>>>4;
+assign mixed   = sum[WM-1:WM-WOUT];
+
 // Apply gain
 always @(posedge clk) if(cen) begin
-    ch0_amp <= g0 * ch0;
-    ch1_amp <= g1 * ch1;
-    ch2_amp <= g2 * ch2;
-    ch3_amp <= g3 * ch3;
+    ch0_amp <= ch0_pre[WM+7:8];
+    ch1_amp <= ch1_pre[WM+7:8];
+    ch2_amp <= ch2_pre[WM+7:8];
+    ch3_amp <= ch3_pre[WM+7:8];
 
-    // divides by 16 to take off the decimal part and leave only
-    // the integer part
-    sum     <= (scaled0 + scaled1 + scaled2 + scaled3)>>>4;
-    limited <= sum>max_pos ? max_pos : (sum<~max_pos ? ~max_pos : sum);
-    mixed   <= limited[wout-1:0];
+    sum     <= pre_sum > MAXPOS ? MAXPOS[WM-1:0] : (
+               pre_sum < MAXNEG ? MAXNEG[WM-1:0] :pre_sum[WM-1:0] );
 end
 
 endmodule // jtframe_mixer
 
-module jtframe_limamp #(parameter win=16,wout=20)(
+module jtframe_limamp #(parameter WIN=16,WOUT=16)(
     input                    clk,
     input                    cen,
     // input signals
-    input signed [win-1:0]   sndin,
+    input signed [WIN-1:0]   sndin,
     // gain for each channel in 4.4 fixed point format
     input  [7:0]             gain,
-    output signed [wout-1:0] sndout
+    output signed [WOUT-1:0] sndout
 );
 
-jtframe_mixer #(.w0(win),.wout(wout)) u_amp(
+jtframe_mixer #(.W0(WIN),.WOUT(WOUT)) u_amp(
     .clk    ( clk       ),
     .cen    ( cen       ),
     .ch0    ( sndin     ),
