a5ca60ec18d79c461b941ec6dfb978f15fca1c0b
Jose Tejada <jotego@gmail.com>
2020-11-25 08:27:15 +0100

    Fixed blank file

--- hdl/sound/firjt49.m
+++ hdl/sound/firjt49.m
@@ -10,6 +10,6 @@ printf("Stopband attenuation %d dB\n", N*22*(f2-f1))
 hc_fft=abs(fft(hc));
 hdb=20*log10(hc_fft);
 haux=hc';
-save filter49 haux
+save firjt49 haux
 printf("Gain at DC %d dB = %d \n", hdb(1), hc_fft(1))
 # use freqz(hc) to display the frequency response
