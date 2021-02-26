d13bf18c9cd0e90075b19b41355a5f6552f2a452
jotego <jotego@gmail.com>
2021-02-26 09:49:14 +0100

    typo

--- hdl/sdram/jtframe_sdram_bank.f
+++ hdl/sdram/jtframe_sdram_bank.f
@@ -5,9 +5,9 @@ jtframe_ram_rq.v
 jtframe_romrq.v
 jtframe_ram_2slots.v
 jtframe_ram_3slots.v
+jtframe_ram_4slots.v
 jtframe_rom_1slot.v
 jtframe_rom_2slots.v
 jtframe_rom_3slots.v
-jtframe_rom_4slots.v
 jtframe_rom_sync.v
 jtframe_sdram_stats.v
