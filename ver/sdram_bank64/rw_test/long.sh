592e20428f72b9da4409766efdbf2f421ab2ea81
Jose Tejada <jotego@gmail.com>
2021-04-29 12:52:23 +0200

    minor

--- ver/sdram_bank64/rw_test/long.sh
+++ ver/sdram_bank64/rw_test/long.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
 CMD="parallel --halt now,fail=1 "
-PERIODS="10 15.6"
+PERIODS="10 15.6 20.8"
+TIME=500
 
 while [ $# -gt 0 ]; do
     case $1 in
@@ -10,6 +11,9 @@ while [ $# -gt 0 ]; do
             exit 0;;
         -dry-run|-dry)
             CMD="$CMD --dry-run";;
+        -time)
+            shift
+            TIME=$1;;
         *)
             echo "Unsupported argument: $1"
             exit 1;;
@@ -17,16 +21,10 @@ while [ $# -gt 0 ]; do
     shift
 done
 
-# 15.6 = 64MHz operation
-#  9   =111MHz operation
-# bank combinations
-$CMD sim.sh -nodump -time 10 -period  ::: $PERIODS ::: -1banks -2banks -3banks -4banks || exit $?
-
 # Special cases
-$CMD sim.sh -nodump -time 10 -period  ::: $PERIODS ::: -norefresh -perf || exit $?
+$CMD sim.sh -nodump -time 25 -period  ::: $PERIODS ::: -norefresh -perf || exit $?
 
 #different bank lengths
-$CMD sim.sh -nodump -period {5} -time 100 -idle {6} \
+$CMD sim.sh -nodump -period {5} -time $TIME -idle {6} \
 -len 0 {1} -len 1 {2} -len 2 {3} -len 3 {4} \
 ::: 16 32 64 ::: 16 32 64 ::: 16 32 64 ::: 16 32 64 ::: $PERIODS ::: 10 50 90 || exit $?
-
