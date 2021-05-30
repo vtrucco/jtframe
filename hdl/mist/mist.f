8abddd40a30fb24c0332db5469860006792741b3
jotego <jotego@gmail.com>
2021-05-30 09:40:19 +0200

    Sim files

--- hdl/mist/mist.f
+++ hdl/mist/mist.f
@@ -14,8 +14,6 @@ data_io.v
 ../ver/spitx.v
 ../ver/video_dump.v
 ../ver/jtframe_romrq_rdy_check.v
-../video/jtgng_vga.v
-../video/jtgng_vgapxl.v
 ../video/jtframe_scan2x.v
 ../mister/sys/video_mixer.sv
 ../mister/sys/video_cleaner.sv
