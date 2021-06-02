446bf034866fdea03ff656538b904c9a213edb86
Jose Tejada <jotego@gmail.com>
2021-06-02 08:55:54 +0200

    wip

--- hdl/jtframe_cheat.v
+++ hdl/jtframe_cheat.v
@@ -44,49 +44,97 @@ parameter [AW-1:0] CHEAT_ADDR==0,
     output [ 15:0]  ba0_din,
     output [  1:0]  ba0_din_m,
 
-    input           ba0_rdy
+    input           ba0_rdy,
+
+    // PBlaze Program
+    input           prog_en,      // resets the address counter
+    input           prog_wr,      // strobe for new data
+    input  [7:0]    prog_data
 );
 
 wire clk = clk_rom;
 
-reg       last_LVBL, pending, lock, wrcheat;
-reg [5:0] frame_cnt;
+// Instruction ROM
+wire [11:0] iaddr;
+wire [ 7:0] idata;
 
-assign ba0_addr  = lock ? CHEAT_ADDR : game_addr;
-assign ba0_rd    = lock ? 1'b0 : game_rd;
-assign ba0_wr    = lock ? wrcheat : game_wr;
-assign ba0_din   = lock ? CHEAT_VAL : game_din;
-assign ba0_din_m = lock ? CHEAT_MASK : game_din_m;
-assign game_rdy  = lock ? 1'b0 : ba0_rdy;
+// Ports
+wire [ 7:0] pout, paddr;
+reg  [17:0] pin;
+wire        pwr, kwr, prd;
 
-always @(posedge clk, rst) begin
-    if(rst) begin
-       last_LVBL <= 0;
-       lock      <= 0;
-       pending   <= 0;
-       wrcheat   <= 0;
-       frame_cnt <= 0;
+// interrupts
+reg         irq;
+wire        iack;
+
+pauloBlaze u_blaze(
+    .clk            ( clk       ),
+    .reset          ( rst       ),
+    .sleep          ( 1'b0      ),
+
+    .address        ( iaddr     ),
+    .instruction    ( idata     ),
+    .bram_enable    (           ),
+
+    .in_port        ( pin       ),
+    .out_port       ( pout      ),
+    .port_id        ( paddr     ),
+    .write_strobe   ( pwr       ),
+    .k_write_strobe ( kwr       ),
+    .read_strobe    ( prd       ),
+
+    .interrupt      ( irq       ),
+    .interrupt_ack  ( iack      )
+);
+
+// 8 to 18 bit conversion
+reg  [15:0] prog_fifo;
+reg  [ 8:0] st;
+reg         last_en, prog_post;
+reg  [17:0] prog_word;
+
+always @(posedge clk) begin
+    last_en <= prog_en;
+    if( prog_en & ~last_en ) begin
+        word_cnt  <= 0;
+        prog_post <= 0;
     end else begin
-        last_LVBL <= LVBL;
-
-        if( !LVBL && last_LVBL ) begin
-            pending   <= enable && frame_cnt==0;
-            frame_cnt <= frame_cnt==FRAME_CNT ? 6'd0 : frame_cnt + 1'd1;
+        if( prog_wr ) begin
+            prog_fifo <= { prog_data, prog_fifo[15:8] };
+            word_cnt  <= word_cnt[3] ? 4'd0 : word_cnt + 4'd1;
+            case( word_cnt )
+                2: begin
+                    word_we   <= 1;
+                    prog_word <= { prog_data[1:0], prog_fifo };
+                end
+                4: begin
+                    word_we   <= 1;
+                    prog_word <= { prog_data[3:0], prog_fifo[15:2] };
+                end
+                6: begin
+                    word_we   <= 1;
+                    prog_word <= { prog_data[5:0], prog_fifo[15:4] };
+                end
+                8: begin
+                    word_we   <= 1;
+                    prog_word <= { prog_data[7:0], prog_fifo[15:6] };
+                end
+                default: word_we <= 0;
+            endcase
+        end else begin
+            word_we <= 0;
         end
-
-        if( pending && ba0_rdy ) begin
-            lock    <= 1;
-            wrcheat <= 1;
-            pending <= 0;
-        end
-
-        if( lock && ba0_ack ) begin
-            wrcheat <= 0;
-        end
-
-        if( lock && ba0_rdy ) lock <= 0;
-
     end
 end
+
+jtframe_prom #(.dw(18),aw(12)) u_irom(
+    .clk    ( clk       ),
+    .cen    ( 1'b1      ),
+    .data   ( prog_word ),
+    .rd_addr( iaddr     ),
+    .wr_addr( prog_addr ),
+    .we     ( word_we   ),
+    .q      ( idata     )
+);
 
 endmodule
