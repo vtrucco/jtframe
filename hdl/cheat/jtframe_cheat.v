ba0e103446791559208bffe6202b61bcaa43855a
jotego <jotego@gmail.com>
2021-06-03 16:50:21 +0200

    Corrects download error

--- hdl/cheat/jtframe_cheat.v
+++ hdl/cheat/jtframe_cheat.v
@@ -14,7 +14,7 @@
 
     Author: Jose Tejada Gomez. Twitter: @topapate
     Version: 1.0
-    Date: 25-9-2019 */
+    Date: 1-6-2021 */
 
 module jtframe_cheat #(parameter AW=22)(
     input   rst,
@@ -201,60 +201,14 @@ pauloBlaze u_blaze(
     .interrupt_ack  ( iack      )
 );
 
-// 8 to 18 bit conversion
-reg  [15:0] prog_fifo;
-reg  [ 8:0] st;
-reg         last_en, prog_post;
-reg  [17:0] prog_word;
-reg         word_we;
-reg  [ 3:0] word_cnt;
-reg  [CHEATW-1:0] prog_addr;
-
-always @(posedge clk) begin
-    last_en <= prog_en;
-    if( prog_en & ~last_en ) begin
-        word_cnt  <= 0;
-        prog_post <= 0;
-        prog_addr <= 0;
-        prog_word <= 0;
-    end else begin
-        if( prog_wr & prog_en ) begin
-            prog_fifo <= { prog_data, prog_fifo[15:8] };
-            word_cnt  <= word_cnt[3] ? 4'd0 : word_cnt + 4'd1;
-            case( word_cnt )
-                2: begin
-                    word_we   <= 1;
-                    prog_word <= { prog_data[1:0], prog_fifo };
-                end
-                4: begin
-                    word_we   <= 1;
-                    prog_word <= { prog_data[3:0], prog_fifo[15:2] };
-                end
-                6: begin
-                    word_we   <= 1;
-                    prog_word <= { prog_data[5:0], prog_fifo[15:4] };
-                end
-                8: begin
-                    word_we   <= 1;
-                    prog_word <= { prog_data[7:0], prog_fifo[15:6] };
-                end
-                default: word_we <= 0;
-            endcase
-        end else begin
-            word_we <= 0;
-        end
-        if( word_we ) prog_addr <= prog_addr+1'd1;
-    end
-end
-
-jtframe_prom #(.dw(18),.aw(CHEATW),.simhex("cheat.hex")) u_irom(
-    .clk    ( clk       ),
-    .cen    ( 1'b1      ),
-    .data   ( prog_word ),
-    .rd_addr( iaddr[CHEATW-1:0] ),
-    .wr_addr( prog_addr ),
-    .we     ( word_we   ),
-    .q      ( idata     )
+jtframe_cheat_rom #(.AW(CHEATW)) u_rom(
+    .clk        ( clk       ),
+    .iaddr      ( iaddr     ),
+    .idata      ( idata     ),
+    // PBlaze Program
+    .prog_en    ( prog_en   ),      // resets the address counter
+    .prog_wr    ( prog_wr   ),      // strobe for new data
+    .prog_data  ( prog_data )
 );
 
 endmodule
---
+++ ver/cheatdwn/sim.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+ncverilog test.v ../../hdl/cheat/jtframe_cheat_rom.v ../../hdl/ram/jtframe_prom.v +access+r +nc64bit
